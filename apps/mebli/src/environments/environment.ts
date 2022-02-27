6d09374a191e3e59083c2a8dd98f9cb917580edd
Marius Schulze <marius.schulze@brainbits.net>
2022-02-27 15:12:59 +0100

    feat: provide environment variables

--- apps/mebli/src/environments/environment.ts
+++ apps/mebli/src/environments/environment.ts
@@ -4,6 +4,14 @@
 
 export const environment = {
   production: false,
+  firebase: {
+    projectId: 'mebli-wbh',
+    appId: '1:629456636827:web:f45444bd505b7335f8af3e',
+    storageBucket: 'mebli-wbh.appspot.com',
+    apiKey: 'AIzaSyA-T6PepxJLCG-6hNp2HnMzsRQronSO7vU',
+    authDomain: 'mebli-wbh.firebaseapp.com',
+    messagingSenderId: '629456636827',
+  },
 };
 
 /*
