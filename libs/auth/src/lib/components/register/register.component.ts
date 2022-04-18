e61a747f4939dc600a9bb02b126bb1bb24281794
Marius Schulze <marius.schulze@t-online.de>
2022-04-18 23:05:49 +0200

    fix: small responsive design adjustments

--- libs/auth/src/lib/components/register/register.component.ts
+++ libs/auth/src/lib/components/register/register.component.ts
@@ -19,16 +19,18 @@ export class RegisterComponent {
     public passwordConfirm = '';
     public passwordsMatch = true;
     public specialcase = false;
-    public PasswordInvalid=false;
-    
+    public passwordInvalid = false;
+
     public constructor(private readonly authService: AuthService, private readonly db: DbService) {}
 
     public async onRegister(): Promise<void> {
         this.passwordsMatch = this.password === this.passwordConfirm;
         if (!this.passwordsMatch || !this.specialcase || !this.lowercase || !this.uppercase || !this.number) {
-            console.log("invalid passwort")
-            this.PasswordInvalid=true;
+            console.log('invalid passwort');
+            this.passwordInvalid = true;
             return;
+        } else {
+            this.passwordInvalid = false;
         }
 
         const accountNameAlreadyTaken = await this.db.docExists<Profile>(
@@ -44,42 +46,27 @@ export class RegisterComponent {
         }
     }
 
-   
-    public onKey(): void  {
-        if (this.password.length>7)
-    {
-        this.lenght=true; 
-    }
-    else  this.lenght=false; 
-     if(this.password.match("(?=.*[0-9])"))
-    {
-        this.number=true;
-    }
-    else this.number=false;
+    public onKey(): void {
+        if (this.password.length > 7) {
+            this.lenght = true;
+        } else this.lenght = false;
 
-    if(this.password.match("(?=.*[A-Z])"))
-    {
-        this.uppercase=true;
-    }
-    else this.uppercase=false;
+        if (this.password.match('(?=.*[0-9])')) {
+            this.number = true;
+        } else this.number = false;
 
-    if(this.password.match("(?=.*[a-z])"))
-    {
-        this.lowercase=true;
-    }
-    else this.lowercase=false;
+        if (this.password.match('(?=.*[A-Z])')) {
+            this.uppercase = true;
+        } else this.uppercase = false;
 
+        if (this.password.match('(?=.*[a-z])')) {
+            this.lowercase = true;
+        } else this.lowercase = false;
 
-    if(this.password.match("(?=.*\\W)"))
-    {
-        this.specialcase=true;
+        if (this.password.match('(?=.*\\W)')) {
+            this.specialcase = true;
+        } else this.specialcase = false;
     }
-    else this.specialcase=false;
-    
-    }
-    
-
-  
 
     public async onLoginWithGoogle(): Promise<void> {
         await this.authService.loginWithGoogle();
